043eebddc923d86cc45d8fc87083e2678e4e0317
jmindrup <jmindrup@iastate.edu>
2023-11-14 19:23:22 -0600

    tests

--- src/App.js
+++ src/App.js
@@ -2,8 +2,7 @@ import {React, useState, useEffect} from 'react';
 import './App.css';
 import Joke from './Joke';
 import DeckList from './DeckList';
-import { Button, Drawer } from 'antd';
-import { Header } from 'antd/es/layout/layout';
+import { FloatButton, Drawer} from 'antd';
 function App() {
 const [deck, setDeck] = useState([]);
 const [images, setImages] = useState([]);
@@ -12,6 +11,40 @@ const [eggs, setEggs] = useState(0);
 const [cardsInDeck, setCardsInDeck] = useState(0);
 const [showDeck, setShowDeck] = useState(false);
 useEffect(() => {
+  if (typeof require.context === 'undefined') {
+    const fs = require('fs');
+    const path = require('path');
+  
+    require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) => {
+      const files = {};
+  
+      function readDirectory(directory) {
+        fs.readdirSync(directory).forEach((file) => {
+          const fullPath = path.resolve(directory, file);
+  
+          if (fs.statSync(fullPath).isDirectory()) {
+            if (scanSubDirectories) readDirectory(fullPath);
+  
+            return;
+          }
+  
+          if (!regularExpression.test(fullPath)) return;
+  
+          files[fullPath] = true;
+        });
+      }
+  
+      readDirectory(path.resolve(__dirname, base));
+  
+      function Module(file) {
+        return require(file);
+      }
+  
+      Module.keys = () => Object.keys(files);
+  
+      return Module;
+    };
+  }
    let imgs = importAll(require.context('./cardthumbnails', false, /\.(png|jpe?g|svg)$/));
    setImages(imgs);
 }, []);
@@ -36,7 +69,7 @@ function importAll(r){
 }
   return (
     <div className="App">
-      <Header><Button onClick={() => setShowDeck(!showDeck)}>Show/Hide Deck</Button></Header>
+      <FloatButton onClick={() => setShowDeck(!showDeck)}>Show/Hide Deck</FloatButton>
       <Drawer mask={false} autoFocus={false} placement='bottom' onClose={() => setShowDeck(false)} open={showDeck}><DeckList removeEgg={removeEgg} removeCard={removeCard} cardsInDeck={cardsInDeck} cardsMap={cardsMap} updateDeck={updateDeck} deck = {deck} images={images}/></Drawer>
       <Joke removeEgg={removeEgg} removeCard={removeCard} cardsMap={cardsMap} updateDeck={updateDeck} deck = {deck} images={images}/>
     </div>
