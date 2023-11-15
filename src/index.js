67a04c38c3a289f37f39ecd47c2f667e6236b893
jmindrup <jmindrup@iastate.edu>
2023-11-14 20:57:14 -0600

    publish

--- src/index.js
+++ src/index.js
@@ -2,17 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import {HashRouter as Router} from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Router>
-    <React>
+  <React.StrictMode>
     <App />
-  </React>
-  </Router>
-  
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
