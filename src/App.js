9d10205898f18bbb0cf62776e37f7c100cd382d8
jmindrup <jmindrup@iastate.edu>
2023-11-14 19:28:43 -0600

    frick tests

--- src/App.js
+++ src/App.js
@@ -11,40 +11,7 @@ const [eggs, setEggs] = useState(0);
 const [cardsInDeck, setCardsInDeck] = useState(0);
 const [showDeck, setShowDeck] = useState(false);
 useEffect(() => {
-  if (typeof require.context === 'undefined') {
-    const fs = require('fs');
-    const path = require('path');
   
-    require.context = (base = '.', scanSubDirectories = false, regularExpression = /\.js$/) => {
-      const files = {};
-  
-      function readDirectory(directory) {
-        fs.readdirSync(directory).forEach((file) => {
-          const fullPath = path.resolve(directory, file);
-  
-          if (fs.statSync(fullPath).isDirectory()) {
-            if (scanSubDirectories) readDirectory(fullPath);
-  
-            return;
-          }
-  
-          if (!regularExpression.test(fullPath)) return;
-  
-          files[fullPath] = true;
-        });
-      }
-  
-      readDirectory(path.resolve(__dirname, base));
-  
-      function Module(file) {
-        return require(file);
-      }
-  
-      Module.keys = () => Object.keys(files);
-  
-      return Module;
-    };
-  }
    let imgs = importAll(require.context('./cardthumbnails', false, /\.(png|jpe?g|svg)$/));
    setImages(imgs);
 }, []);
